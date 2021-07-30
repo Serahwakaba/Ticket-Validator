c52e732c4b0d5f97c8821065910601d7d12089ef
user <user@users-MacBook-Air.local>
2021-07-30 13:10:04 +0300

    login complete

--- app/src/main/java/com/example/ticketvalidator/Login.java
+++ app/src/main/java/com/example/ticketvalidator/Login.java
@@ -2,13 +2,18 @@ package com.example.ticketvalidator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+}}
--- app/src/main/java/com/example/ticketvalidator/MainActivity.java
+++ app/src/main/java/com/example/ticketvalidator/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ticketvalidator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                //Create the intent to start another activity
+                Intent intent = new Intent(view.getContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
