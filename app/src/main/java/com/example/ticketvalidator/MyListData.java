e471802424f79821458753852c341676a105656b
user <user@users-MacBook-Air.local>
2021-08-02 10:15:00 +0300

    model created

---
+++ app/src/main/java/com/example/ticketvalidator/MyListData.java
@@ -0,0 +1,10 @@
+package com.example.ticketvalidator;
+
+public class MyListData {
+    private String id;
+    private String name;
+    private String date;
+    private String total;
+    private String tickets;
+
+}
