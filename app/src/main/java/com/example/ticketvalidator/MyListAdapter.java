d60a89b80f6084657f9a28d10b873ff58da1a9cc
user <user@users-MacBook-Air.local>
2021-08-02 11:19:42 +0300

    custom adapter complete

--- app/src/main/java/com/example/ticketvalidator/MyListAdapter.java
+++ app/src/main/java/com/example/ticketvalidator/MyListAdapter.java
@@ -3,6 +3,7 @@ package com.example.ticketvalidator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.content.Context;
 import java.util.List;
@@ -21,7 +22,7 @@ public class MyListAdapter  extends RecyclerView.Adapter<MyListAdapter.ViewHolde
     }
 
 
-    @NonNull
+  @NonNull
     @Override
     public MyListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
@@ -39,12 +40,32 @@ public class MyListAdapter  extends RecyclerView.Adapter<MyListAdapter.ViewHolde
         holder.date.setText(myListData.getDate());
         holder.number.setText(myListData.getTotal());
         holder.ticket.setText(myListData.getTickets());
-        holder.image.setText(myListData.getId());
+//        holder.image.setImageIcon(myListData.getId());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView name;
+        public TextView date;
+        public TextView number;
+        public TextView ticket;
+        public ImageView image;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            date = itemView.findViewById(R.id.date);
+            number = itemView.findViewById(R.id.number);
+            ticket = itemView.findViewById(R.id.ticket);
+            image = itemView.findViewById(R.id.image);
+
+        }
     }
 }
