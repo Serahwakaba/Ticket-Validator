47fa0dba7efafaf76cd9d65d0c5a9e61f59eefbc
user <user@users-MacBook-Air.local>
2021-08-02 10:18:30 +0300

    constructor commit

--- app/src/main/java/com/example/ticketvalidator/MyListData.java
+++ app/src/main/java/com/example/ticketvalidator/MyListData.java
@@ -7,4 +7,23 @@ public class MyListData {
     private String total;
     private String tickets;
 
+    public MyListData(String id, String name, String date, String total, String tickets) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.total = total;
+        this.tickets = tickets;
+    }
+
+
+
+
+
+    public MyListData(){
+
+    }
+
+
+
+
 }
