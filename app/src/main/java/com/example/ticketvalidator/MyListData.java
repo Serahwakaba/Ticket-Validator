58aebf27114abcabbd777a1656998bb98b08c232
user <user@users-MacBook-Air.local>
2021-08-02 10:20:29 +0300

    getters and setters commit

--- app/src/main/java/com/example/ticketvalidator/MyListData.java
+++ app/src/main/java/com/example/ticketvalidator/MyListData.java
@@ -7,6 +7,12 @@ public class MyListData {
     private String total;
     private String tickets;
 
+
+    public MyListData(){
+
+    }
+
+
     public MyListData(String id, String name, String date, String total, String tickets) {
         this.id = id;
         this.name = name;
@@ -19,9 +25,49 @@ public class MyListData {
 
 
 
-    public MyListData(){
 
+
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(String tickets) {
+        this.tickets = tickets;
+    }
+
 
 
 
